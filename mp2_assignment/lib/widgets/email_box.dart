fd7c33befa448e128d542e6aabdc031f8d2df8d1
ChrisBerarducci <125297379+ChrisB203@users.noreply.github.com>
2025-10-15 14:10:11 -0400

    Email Box Outline Done
    Completed Email Box minimal outline, time to get the json file in

--- mp2_assignment/lib/widgets/email_box.dart
+++ mp2_assignment/lib/widgets/email_box.dart
@@ -16,7 +16,7 @@ class _EmailBoxState extends State<EmailBox> {
       width: double.infinity,
       decoration: BoxDecoration(
         border: Border(top: BorderSide(color: Colors.grey, width: 2)),
-        color: Colors.grey[200],
+        color: const Color.fromARGB(255, 231, 231, 231),
       ),
       child: Padding(
         padding: EdgeInsets.fromLTRB(wPad, 10, wPad, 10),
@@ -37,16 +37,59 @@ class _EmailBoxState extends State<EmailBox> {
             ),
 
             //End Icon Box
+
+            //Quick Spacer
             SizedBox(width: 20),
-            //Column Subset
+
+            //Non Icon Section
             Expanded(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  //Name Row
                   Row(
                     children: [
-                      Text('name'),
+                      Text(
+                        'Name',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1.25,
+                        ),
+                      ),
                       Spacer(),
-                      Text('time'),
+                      Text(
+                        'time',
+                        style: TextStyle(height: 1.25, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+
+                  //Header
+                  Text('Header', style: TextStyle(height: 1)),
+
+                  //Body Row
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'This kas  l aslkgjaslkgjsaflnaslk sfjlkasf asfkjfalfjlkaasfkjfalfjlka ',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            height: 1.25,
+                          ),
+                        ),
+                        //arrow_forward_ios
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Colors.grey[500],
+                        weight: 5,
+                      ),
                     ],
                   ),
                 ],
